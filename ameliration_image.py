a4b1d40fea7cacacb14cdb29b49fdbf99b5a0535
Nathan <153100527+Akanaifu@users.noreply.github.com>
2025-11-30 11:52:36 +0100

    ajout d'une option d'affichage dans la fonction ameliorer_image et suppression du fichier traitement_images.py

--- ameliration_image.py
+++ ameliration_image.py
@@ -35,7 +35,7 @@ import numpy as np
 # plt.show()
 
 
-def ameliorer_image(img_path: str) -> np.ndarray:
+def ameliorer_image(img_path: str, afficher_img: bool = False) -> np.ndarray:
     """
     Prend une image (NumPy array), applique:
       - conversion en niveaux de gris
@@ -48,21 +48,53 @@ def ameliorer_image(img_path: str) -> np.ndarray:
     """
 
     # Convertit en niveaux de gris si l'image est couleur
-    I = plt.imread(img_path)
-    img = skim.color.rgb2gray(I)
-
     # Pipeline simple
+    I = plt.imread(img_path)
+
+    # Vérifier si l'image est en couleur ou en niveaux de gris
+    if len(I.shape) == 3:  # Image couleur (RGB)
+        img = skim.color.rgb2gray(I)
+    else:  # Image déjà en niveaux de gris
+        img = I
+
     I_inv = skim.util.invert(img)
     I_inv_corrigee = skim.restoration.denoise_tv_chambolle(I_inv)
     I_corrigee = skim.util.invert(I_inv_corrigee)
     I_gamma = skim.exposure.adjust_gamma(I_corrigee, 0.5)
     I_hist = skim.exposure.equalize_hist(I_gamma)
 
-    return I_hist
+    if afficher_img:
+        plt.subplot(2, 4, 1)
+        plt.imshow(I, cmap="gray")
+        plt.title("Image originale")
+        plt.subplot(2, 4, 2)
+        plt.imshow(img, cmap="gray")
+        if len(I.shape) == 3:
+            plt.title("Niveaux de gris")
+            plt.subplot(2, 4, 2)
+        else:
+            plt.imshow(img, cmap="gray")
+            plt.title("Image (déjà en gris)")
+        plt.subplot(2, 4, 3)
+        plt.imshow(I_inv, cmap="gray")
+        plt.title("Inversée")
+        plt.subplot(2, 4, 4)
+        plt.imshow(I_inv_corrigee, cmap="gray")
+        plt.title("Débruitée (TV)")
+        plt.subplot(2, 4, 5)
+        plt.imshow(I_corrigee, cmap="gray")
+        plt.title("Ré-inversée")
+        plt.subplot(2, 4, 6)
+        plt.imshow(I_gamma, cmap="gray")
+        plt.title("Correction gamma")
+        plt.subplot(2, 4, 7)
+        plt.imshow(I_hist, cmap="gray")
+        plt.title("Égalisation histogramme")
+        plt.show()
+
+    return I_corrigee
 
 
 if __name__ == "__main__":
-    I = plt.imread("./MEDIA/IMG/video_003_008.jpg")
-    I_amelioree = ameliorer_image(I)
-    plt.imshow(I_amelioree, cmap="gray")
-    plt.show()
+    for i in range(1, 7):
+        I_amelioree = ameliorer_image(f"./MEDIA/IMG/video_12_00{i}.jpg", True)
