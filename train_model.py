68ba84e72356a73beaacb7a349f6f579fefd9428
Cyril Lamand <crocrodile@debian-server.local>
2025-12-08 17:52:47 +0100

    Ajout de tout les trains

--- train_model.py
+++ train_model.py
@@ -1,18 +1,18 @@
 from ultralytics import YOLO
 
 # Charger le modèle pré-entraîné
-model = YOLO("yolov8n.pt")
+model = YOLO("runs/detect/train2/weights/last.pt")
 
 # Entraîner le modèle
 results = model.train(
     data="mon_dataset/FallDown.v3-person_bed.yolov8/data.yaml",
-    epochs=5,
+    epochs=200,
     imgsz=640,
     batch=16,  # Ajustez selon votre mémoire GPU/CPU
     name="falldown_detection",  # Nom du dossier de résultats
     project="runs/detect",  # Dossier principal des résultats
     device="cpu",  # Utilisation du CPU (pas de GPU CUDA disponible)
-    patience=50,  # Early stopping
+    patience=150,  # Early stopping
     save=True,
     plots=True,
 )
